02d44bc9c571419e35fd0b6a25c67a870447ed94
Ricky Su <rickys@xilinx.com>
2020-12-23 05:40:45 -0800

    add ref files

---
+++ docs/ZynqMPSoC-EDT/ref_files/system_project_example/testapp_r5.c
@@ -0,0 +1,388 @@
+/******************************************************************************
+*
+* Copyright (C) 2010 - 2014 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+/*
+ * helloworld.c: simple test application
+ *
+ * This application configures UART 16550 to baud rate 9600.
+ * PS7 UART (Zynq) is not initialized by this application, since
+ * bootrom/bsp configures it to baud rate 115200
+ *
+ * ------------------------------------------------
+ * | UART TYPE   BAUD RATE                        |
+ * ------------------------------------------------
+ *   uartns550   9600
+ *   uartlite    Configurable only in HW design
+ *   ps7_uart    115200 (configured by bootrom/bsp)
+ */
+
+/***************************** Include Files *******************************/
+#include <stdio.h>
+//#include "platform.h"
+#include "math.h"
+#include "xparameters.h"
+#include "xplatform_info.h"
+#include "xuartps.h"
+#include "xil_exception.h"
+#include "xil_printf.h"
+
+#ifdef XPAR_INTC_0_DEVICE_ID
+#include "xintc.h"
+#else
+#include "xscugic.h"
+#endif
+/************************** Constant Definitions **************************/
+
+/*
+ * The following constants map to the XPAR parameters created in the
+ * xparameters.h file. They are defined here such that a user can easily
+ * change all the needed parameters in one place.
+ */
+#ifdef XPAR_INTC_0_DEVICE_ID
+#define INTC		XIntc
+#define UART_DEVICE_ID		XPAR_XUARTPS_0_DEVICE_ID
+#define INTC_DEVICE_ID		XPAR_INTC_0_DEVICE_ID
+#define UART_INT_IRQ_ID		XPAR_INTC_0_UARTPS_0_VEC_ID
+#else
+#define INTC		XScuGic
+#define UART_DEVICE_ID		XPAR_XUARTPS_1_DEVICE_ID
+#define INTC_DEVICE_ID		XPAR_SCUGIC_SINGLE_DEVICE_ID
+#define UART_INT_IRQ_ID		XPAR_XUARTPS_1_INTR
+#endif
+
+/************************** Function Prototypes *****************************/
+
+int PsUartSetup(INTC *IntcInstPtr, XUartPs *UartInstPtr,
+			u16 DeviceId, u16 UartIntrId);
+
+
+static int SetupInterruptSystem(INTC *IntcInstancePtr,
+				XUartPs *UartInstancePtr,
+				u16 UartIntrId);
+
+void Handler(void *CallBackRef, u32 Event, unsigned int EventData);
+
+/************************** Variable Definitions ***************************/
+
+XUartPs UartPs	;		/* Instance of the UART Device */
+INTC InterruptController;	/* Instance of the Interrupt Controller */
+
+/*****************************************************************************/
+/**
+* This function is the main function for this application.
+*
+* @param	None.
+*
+* @return	XST_SUCCESS to indicate success, else XST_FAILURE to indicate a
+*		Failure.
+*
+* @note		None.
+*
+******************************************************************************/
+int main()
+{
+    int Status;
+
+    Status = PsUartSetup(&InterruptController, &UartPs,
+				UART_DEVICE_ID, UART_INT_IRQ_ID);
+	if (Status != XST_SUCCESS) {
+		xil_printf("UART Interrupt application Failed\r\n");
+		return XST_FAILURE;
+	}
+
+	while(1)
+    {
+    	printf("\nHello World from R5-0 displayed on UART-1\n");
+    	Xil_ExceptionDisable();
+    	asm volatile ("wfi");
+    	Xil_ExceptionEnable();
+    }
+    return XST_SUCCESS;
+}
+
+/**************************************************************************/
+/**
+*
+* This function does a setup of the UartPS device and driver. The purpose of
+* this function is to illustrate how to use the XUartPs driver.
+*
+* This function uses interrupt mode of the device.
+*
+* @param	IntcInstPtr is a pointer to the instance of the Scu Gic driver.
+* @param	UartInstPtr is a pointer to the instance of the UART driver
+*		which is going to be connected to the interrupt controller.
+* @param	DeviceId is the device Id of the UART device and is typically
+*		XPAR_<UARTPS_instance>_DEVICE_ID value from xparameters.h.
+* @param	UartIntrId is the interrupt Id and is typically
+*		XPAR_<UARTPS_instance>_INTR value from xparameters.h.
+*
+* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
+*
+* @note
+*
+* This function contains an infinite loop such that if interrupts are not
+* working it may never return.
+*
+**************************************************************************/
+int PsUartSetup(INTC *IntcInstPtr, XUartPs *UartInstPtr,
+			u16 DeviceId, u16 UartIntrId)
+{
+	int Status;
+	XUartPs_Config *Config;
+	u32 IntrMask;
+
+	if (XGetPlatform_Info() == XPLAT_ZYNQ_ULTRA_MP) {
+#ifdef XPAR_XUARTPS_1_DEVICE_ID
+		DeviceId = XPAR_XUARTPS_1_DEVICE_ID;
+#endif
+	}
+
+	/*
+	 * Initialize the UART driver so that it's ready to use
+	 * Look up the configuration in the config table, then initialize it.
+	 */
+	Config = XUartPs_LookupConfig(DeviceId);
+	if (NULL == Config) {
+		return XST_FAILURE;
+	}
+
+	Status = XUartPs_CfgInitialize(UartInstPtr, Config, Config->BaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/* Check hardware build */
+	Status = XUartPs_SelfTest(UartInstPtr);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Connect the UART to the interrupt subsystem such that interrupts
+	 * can occur. This function is application specific.
+	 */
+	Status = SetupInterruptSystem(IntcInstPtr, UartInstPtr, UartIntrId);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Setup the handlers for the UART that will be called from the
+	 * interrupt context when data has been sent and received, specify
+	 * a pointer to the UART driver instance as the callback reference
+	 * so the handlers are able to access the instance data
+	 */
+	XUartPs_SetHandler(UartInstPtr, (XUartPs_Handler)Handler, UartInstPtr);
+
+	/*
+	 * Enable the interrupt of the UART so interrupts will occur, setup
+	 * a local loopback so data that is sent will be received.
+	 */
+	IntrMask =
+		XUARTPS_IXR_TOUT | XUARTPS_IXR_PARITY | XUARTPS_IXR_FRAMING |
+		XUARTPS_IXR_OVER | XUARTPS_IXR_TXEMPTY | XUARTPS_IXR_RXFULL |
+		XUARTPS_IXR_RXOVR;
+
+	if (UartInstPtr->Platform == XPLAT_ZYNQ_ULTRA_MP) {
+		IntrMask |= XUARTPS_IXR_RBRK;
+	}
+
+	XUartPs_SetInterruptMask(UartInstPtr, IntrMask);
+
+	XUartPs_SetOperMode(UartInstPtr, XUARTPS_OPER_MODE_LOCAL_LOOP);
+
+	/*
+	 * Set the receiver timeout. If it is not set, and the last few bytes
+	 * of data do not trigger the over-water or full interrupt, the bytes
+	 * will not be received. By default it is disabled.
+	 *
+	 * The setting of 8 will timeout after 8 x 4 = 32 character times.
+	 * Increase the time out value if baud rate is high, decrease it if
+	 * baud rate is low.
+	 */
+	XUartPs_SetRecvTimeout(UartInstPtr, 8);
+
+	/* Set the UART in Normal Mode */
+	XUartPs_SetOperMode(UartInstPtr, XUARTPS_OPER_MODE_NORMAL);
+
+	return XST_SUCCESS;
+}
+
+/**************************************************************************/
+/**
+*
+* This function is the handler which performs processing to handle data events
+* from the device.  It is called from an interrupt context. so the amount of
+* processing should be minimal.
+*
+* This handler is application specific and retained as a place holder for
+* this application.
+*
+** @param	CallBackRef contains a callback reference from the driver,
+*		in this case it is the instance pointer for the XUartPs driver.
+* @param	Event contains the specific kind of event that has occurred.
+* @param	EventData contains the number of bytes sent or received for sent
+*		and receive events.
+*
+* @return	None.
+*
+* @note		None.
+*
+***************************************************************************/
+void Handler(void *CallBackRef, u32 Event, unsigned int EventData)
+{
+	//Do nothing.
+}
+
+/*****************************************************************************/
+/**
+*
+* This function sets up the interrupt system so interrupts can occur for the
+* Uart. This function is application-specific. The user should modify this
+* function to fit the application.
+*
+* @param	IntcInstancePtr is a pointer to the instance of the INTC.
+* @param	UartInstancePtr contains a pointer to the instance of the UART
+*		driver which is going to be connected to the interrupt
+*		controller.
+* @param	UartIntrId is the interrupt Id and is typically
+*		XPAR_<UARTPS_instance>_INTR value from xparameters.h.
+*
+* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
+*
+* @note		None.
+*
+****************************************************************************/
+static int SetupInterruptSystem(INTC *IntcInstancePtr,
+				XUartPs *UartInstancePtr,
+				u16 UartIntrId)
+{
+	int Status;
+
+#ifdef XPAR_INTC_0_DEVICE_ID
+#ifndef TESTAPP_GEN
+	/*
+	 * Initialize the interrupt controller driver so that it's ready to
+	 * use.
+	 */
+	Status = XIntc_Initialize(IntcInstancePtr, INTC_DEVICE_ID);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+#endif
+	/*
+	 * Connect the handler that will be called when an interrupt
+	 * for the device occurs, the handler defined above performs the
+	 * specific interrupt processing for the device.
+	 */
+	Status = XIntc_Connect(IntcInstancePtr, UartIntrId,
+		(XInterruptHandler) XUartPs_InterruptHandler, UartInstancePtr);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+#ifndef TESTAPP_GEN
+	/*
+	 * Start the interrupt controller so interrupts are enabled for all
+	 * devices that cause interrupts.
+	 */
+	Status = XIntc_Start(IntcInstancePtr, XIN_REAL_MODE);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+#endif
+	/*
+	 * Enable the interrupt for uart
+	 */
+	XIntc_Enable(IntcInstancePtr, UartIntrId);
+
+	#ifndef TESTAPP_GEN
+	/*
+	 * Initialize the exception table.
+	 */
+	Xil_ExceptionInit();
+
+	/*
+	 * Register the interrupt controller handler with the exception table.
+	 */
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
+				(Xil_ExceptionHandler) XIntc_InterruptHandler,
+				IntcInstancePtr);
+	#endif
+#else
+#ifndef TESTAPP_GEN
+	XScuGic_Config *IntcConfig; /* Config for interrupt controller */
+
+	/* Initialize the interrupt controller driver */
+	IntcConfig = XScuGic_LookupConfig(INTC_DEVICE_ID);
+	if (NULL == IntcConfig) {
+		return XST_FAILURE;
+	}
+
+	Status = XScuGic_CfgInitialize(IntcInstancePtr, IntcConfig,
+					IntcConfig->CpuBaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Connect the interrupt controller interrupt handler to the
+	 * hardware interrupt handling logic in the processor.
+	 */
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
+				(Xil_ExceptionHandler) XScuGic_InterruptHandler,
+				IntcInstancePtr);
+#endif
+
+	/*
+	 * Connect a device driver handler that will be called when an
+	 * interrupt for the device occurs, the device driver handler
+	 * performs the specific interrupt processing for the device
+	 */
+	Status = XScuGic_Connect(IntcInstancePtr, UartIntrId,
+				  (Xil_ExceptionHandler) XUartPs_InterruptHandler,
+				  (void *) UartInstancePtr);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/* Enable the interrupt for the device */
+	XScuGic_Enable(IntcInstancePtr, UartIntrId);
+
+#endif
+#ifndef TESTAPP_GEN
+	/* Enable interrupts */
+	 Xil_ExceptionEnable();
+#endif
+
+	return XST_SUCCESS;
+}
