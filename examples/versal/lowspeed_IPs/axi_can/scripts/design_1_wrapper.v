bfa5168b4f94beda835a5907725d86cb17ffa61e
INAGANTI-VENU <74894579+INAGANTI-VENU@users.noreply.github.com>
2021-09-25 16:32:30 +0530

    Add files via upload

--- examples/versal/lowspeed_IPs/axi_can/scripts/design_1_wrapper.v
+++ examples/versal/lowspeed_IPs/axi_can/scripts/design_1_wrapper.v
@@ -1,20 +1,12 @@
-################################################################
-/* # Copyright 2020 Xilinx Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License. */
-################################################################
-
-   
+//Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2021.1 (lin64) Build 3247384 Thu Jun 10 19:36:07 MDT 2021
+//Date        : Sun Jul 25 21:49:46 2021
+//Host        : xhdapps52 running 64-bit Ubuntu 18.04.1 LTS
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
 `timescale 1 ps / 1 ps
 
 module design_1_wrapper
