f9ef1a3ca3e9285caaeefcc3c657eab9dd810505
Aakrati Chaturvedi <aakrati.chaturvedi@xilinx.com>
2024-01-11 16:34:49 +0530

    Dhrystone tutorial updates

--- docs/Performance/Performance_Benchmark/Dhrystone/Design/design.tcl
+++ docs/Performance/Performance_Benchmark/Dhrystone/Design/design.tcl
@@ -1,19 +1,16 @@
 
-# ########################################################################
-# Copyright (C) 2019, Xilinx Inc - All rights reserved
+# ############################################################################
+# Copyright (C) 2022-2023, Advanced Micro Devices, Inc. All rights reserved. 
+# SPDX-License-Identifier: MIT
+#
+# This is a generated script based on design: edt_versal
+#
+# Though there are limitations about the generated script,
+# the main purpose of this utility is to make learning
+# IP Integrator Tcl commands easier.
+# ############################################################################
 
-# Licensed under the Apache License, Version 2.0 (the "License"). You may
-# not use this file except in compliance with the License. A copy of the
-# License is located at
 
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-# ########################################################################
 
 set design_name dhrystone_perf
 create_bd_design "$design_name"
